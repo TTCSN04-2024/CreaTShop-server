90e38ba5184b35db28f8a96d40f053d2bd11ba01
lehonganh0201 <le2960620@gmail.com>
2024-09-26 10:19:47 +0700

    config: cloudinary

---
+++ creatshop/src/main/java/com/example/creatshop/config/CloudinaryConfig.java
@@ -0,0 +1,41 @@
+package com.example.creatshop.config;
+/*
+ * @author HongAnh
+ * @created 26 / 09 / 2024 - 9:58 AM
+ * @project CreaTShop-server
+ * @social Github: https://github.com/lehonganh0201
+ * @social Facebook: https://www.facebook.com/profile.php?id=100047152174225
+ */
+
+import com.cloudinary.Cloudinary;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CloudinaryConfig {
+    @Value("${cloudinary.cloud_name}")
+    String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, Object> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+        config.put("secure", true);
+        return new Cloudinary(config);
+    }
+}
