2639b9d60fdbcba011925c25244b40912ecd58f6
AnhDev <132396618+lehonganh0201@users.noreply.github.com>
2024-11-05 23:13:45 +0700

    add entity payment, order (#36)

---
+++ creatshop/src/main/java/com/example/creatshop/constant/PaymentStatus.java
@@ -0,0 +1,15 @@
+package com.example.creatshop.constant;
+/*
+ * @author HongAnh
+ * @created 05 / 11 / 2024 - 11:01 CH
+ * @project CreaTShop-server
+ * @social Github: https://github.com/lehonganh0201
+ * @social Facebook: https://www.facebook.com/profile.php?id=100047152174225
+ */
+
+public enum PaymentStatus {
+    PENDING,    // Đang chờ thanh toán
+    COMPLETED,  // Thanh toán thành công
+    FAILED,     // Thanh toán thất bại
+    CANCELED    // Thanh toán bị hủy
+}
---
+++ creatshop/src/main/java/com/example/creatshop/domain/entity/OrderItem.java
@@ -0,0 +1,50 @@
+package com.example.creatshop.domain.entity;
+/*
+ * @author HongAnh
+ * @created 05 / 11 / 2024 - 11:03 CH
+ * @project CreaTShop-server
+ * @social Github: https://github.com/lehonganh0201
+ * @social Facebook: https://www.facebook.com/profile.php?id=100047152174225
+ */
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    OrderDetail orderDetail;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "product_variant")
+    ProductVariant variant;
+
+    Integer quantity;
+
+    @CreationTimestamp
+    Timestamp createdAt;
+
+    @UpdateTimestamp
+    Timestamp updatedAt;
+}
