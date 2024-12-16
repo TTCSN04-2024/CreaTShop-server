743c954479d61f58c969a64b4ffd09571143f8ce
lehonganh0201 <le2960620@gmail.com>
2024-12-16 22:58:11 +0700

    fix validate order item

--- creatshop/src/main/java/com/example/creatshop/domain/dto/request/OrderRequest.java
+++ creatshop/src/main/java/com/example/creatshop/domain/dto/request/OrderRequest.java
@@ -8,6 +8,7 @@ package com.example.creatshop.domain.dto.request;
  */
 
 import com.example.creatshop.constant.ErrorMessage;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -26,6 +27,7 @@ public class OrderRequest {
     @Min(value = 1, message = ErrorMessage.Validate.ERR_PAYMENT_ID_MIN)
     Integer paymentId;
 
+    @Valid
     @NotNull(message = ErrorMessage.Validate.ERR_ORDER_ITEMS_NOT_NULL)
     List<@NotNull(message = ErrorMessage.Validate.ERR_ORDER_ITEM_NOT_NULL) OrderItemRequest> orderItems;
 }
